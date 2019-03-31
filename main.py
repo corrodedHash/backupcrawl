95eda5e32a7cd712e6073f03818659fd5c0d4817
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-03-31 23:32:56 +0200

    Worked on better info gathering

--- main.py
+++ main.py
@@ -1,31 +1,55 @@
 """Main file"""
-from typing import Iterator
+from typing import Iterator, List
+from dataclasses import dataclass
 import os
+
+
+def is_vc_root(path: str) -> bool:
+    """Checks if given path is the root of a git repo"""
+    return os.path.isdir(os.path.join(path, '.git'))
+
+def git_backedup(path: str) -> bool:
+    """Checks if a git repository is pushed and clean"""
+    pass
+
+@dataclass
+class BackupDir():
+    """An entry for the backup scan"""
+    path: str
+    is_git_repo: bool = False
+    git_repo_saved: bool = False
+
 
 
 def breadth_first_file_scan(root: str) -> Iterator[str]:
     """ Iterates directory tree breadth first"""
     dirs = [root]
-    # while we has dirs to scan
-    while dirs:
-        next_dirs = []
+    while True:
+        next_dirs: List[str] = []
+        files: List[str] = []
+        got_vc = False
         for parent in dirs:
-            # scan each dir
             for current_file in os.listdir(parent):
-                # if there is a dir, then save for next ittr
-                # if it  is a file then yield it (we'll return later)
                 current_file_path = os.path.join(parent, current_file)
-                if os.path.isdir(current_file_path):
-                    next_dirs.append(current_file_path)
-                else:
-                    yield current_file_path
-        # once we've done all the current dirs then
-        # we set up the next itter as the child dirs
-        # from the current itter.
-        dirs = next_dirs
+                if not os.path.isdir(current_file_path):
+                    files.append(current_file_path)
+                    continue
+                if is_vc_root(current_file_path):
+                    got_vc = True
+                    continue
+                next_dirs.append(current_file_path)
 
-# -------------------------------------------
-# an example func that just outputs the files.
+        if got_vc:
+            for directory in next_dirs:
+                yield from breadth_first_file_scan(directory)
+            yield from files
+            return
+
+        if not next_dirs:
+            yield root
+            return
+
+        dirs = next_dirs
 
 
 def walkbf(path: str) -> None:
@@ -34,4 +58,4 @@ def walkbf(path: str) -> None:
         print(current_file)
 
 
-walkbf('/home/lukas/')
+walkbf('/home/lukas/documents/')
