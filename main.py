6d3401aaccc04f21d150e4f70e4e9688752e47ff
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-04-03 14:15:57 +0200

    Refactoring

--- main.py
+++ main.py
@@ -3,95 +3,101 @@ from typing import Iterator, List
 from dataclasses import dataclass
 import os
 import subprocess
+import enum
 
 
-def is_vc_protected(path: str) -> bool:
-    """Returns whether a file or a directory is version controlled by git"""
-    if os.path.isfile(path):
-        path = os.path.dirname(os.path.realpath(path))
-    git_status = subprocess.run(
-        ["git", "status", "--porcelain"], cwd=path, stdout=subprocess.PIPE)
-    if git_status.returncode == 0:
-        return True
-    return False
+class GitSyncStatus(enum.Enum):
+    """Different backup states a git repository can be in"""
+    NOGIT = enum.auto()
+    CLEAN_SYNCED = enum.auto()
+    DIRTY = enum.auto()
+    AHEAD = enum.auto()
 
 
-def is_vc_root(path: str) -> bool:
-    """Checks if given path is the root of a git repo"""
-    return os.path.isdir(os.path.join(path, '.git'))
-
-
-def git_is_clean(path: str) -> bool:
-    """Checks if a git repository is clean"""
-    git_status = subprocess.run(
-        ["git", "status", "--porcelain"], cwd=path, stdout=subprocess.PIPE)
-
-    if git_status.returncode != 0:
-        raise RuntimeError
-
-    result: bool = git_status.stdout == b""
-
-    return result
-
-
-def git_pushed(path: str) -> bool:
-    """Checks if a git repository has all local branches pushed"""
+def git_check_ahead(path: str) -> bool:
+    """Checks if a git repository got a branch that is ahead of the remote branch"""
 
     git_for_each = subprocess.run(
-        ["git", "for-each-ref", "--format='%(upstream:trackshort)'", "refs/heads"],
+        ["git", "for-each-ref",
+         "--format='%(upstream:trackshort)'", "refs/heads"],
         cwd=path, stdout=subprocess.PIPE)
 
     if git_for_each.returncode != 0:
         raise RuntimeError
 
-    return all(x in (b"'<'", b"'='") for x in git_for_each.stdout.splitlines())
+    return any(x in (b"'>'", b"'<>'", b"''") for x in git_for_each.stdout.splitlines())
+
+
+def git_check(path: str) -> GitSyncStatus:
+    """Checks if a git repository is clean"""
+    git_status = subprocess.run(
+        ["git", "status", "--porcelain"], cwd=path, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+    if git_status.returncode != 0:
+        return GitSyncStatus.NOGIT
+
+    if git_status.stdout != b"":
+        return GitSyncStatus.DIRTY
+
+    if git_check_ahead(path):
+        return GitSyncStatus.AHEAD
+
+    return GitSyncStatus.CLEAN_SYNCED
 
 
 @dataclass
 class BackupDir():
     """An entry for the backup scan"""
     path: str
-    symlink: bool = False
-    is_git_repo: bool = False
-    repo_clean: bool = False
-    repo_pushed: bool = False
+    git_status: GitSyncStatus = GitSyncStatus.NOGIT
 
 
 def breadth_first_file_scan(root: str) -> Iterator[BackupDir]:
     """ Iterates directory tree breadth first"""
+    if git_check(root) != GitSyncStatus.NOGIT:
+        yield BackupDir(root, git_check(root))
+        return
+
     dirs = [root]
     while True:
         next_dirs: List[str] = []
         files: List[BackupDir] = []
-        got_vc = False
+        split_tree = False
+        tree_empty = True
         for parent in dirs:
             for current_file in os.listdir(parent):
                 current_file_path = os.path.join(parent, current_file)
+
                 if os.path.islink(current_file_path):
-                    files.append(BackupDir(current_file_path, symlink=True))
                     continue
 
                 if not os.path.isdir(current_file_path):
+                    if not os.path.isfile(current_file_path):
+                        # If path is not a directory, or a file,
+                        # it is some symlink, socket or pipe. We don't care
+                        continue
                     files.append(BackupDir(current_file_path))
+                    tree_empty = False
                     continue
 
-                if is_vc_root(current_file_path):
-                    files.append(BackupDir(
-                        current_file_path, is_git_repo=True,
-                        repo_clean=git_is_clean(current_file_path), repo_pushed=git_pushed(current_file_path)))
-                    got_vc = True
+                git_status = git_check(current_file_path)
+                if git_status != GitSyncStatus.NOGIT:
+                    files.append(
+                        BackupDir(current_file_path, git_status=git_status))
+                    split_tree = True
                     continue
 
                 next_dirs.append(current_file_path)
 
-        if got_vc:
+        if split_tree:
             for directory in next_dirs:
                 yield from breadth_first_file_scan(directory)
             yield from files
             return
 
         if not next_dirs:
-            yield BackupDir(root)
+            if not tree_empty:
+                yield BackupDir(root)
             return
 
         dirs = next_dirs
@@ -102,19 +108,18 @@ def walkbf(path: str) -> None:
     clean_repos: List[str] = []
     dirty_repos: List[str] = []
     unsynced_repos: List[str] = []
-    symlinks: List[str] = []
     for current_file in breadth_first_file_scan(path):
-        if current_file.symlink:
-            symlinks.append(current_file.path)
-        elif current_file.is_git_repo:
-            if current_file.repo_clean:
-                clean_repos.append(current_file.path)
-            elif not current_file.repo_pushed:
-                unsynced_repos.append(current_file.path)
-            else:
-                dirty_repos.append(current_file.path)
-        else:
+        if current_file.git_status == GitSyncStatus.NOGIT:
             print("\t" + current_file.path)
+        elif current_file.git_status == GitSyncStatus.CLEAN_SYNCED:
+            clean_repos.append(current_file.path)
+        elif current_file.git_status == GitSyncStatus.AHEAD:
+            unsynced_repos.append(current_file.path)
+        elif current_file.git_status == GitSyncStatus.DIRTY:
+            dirty_repos.append(current_file.path)
+        else:
+            raise RuntimeError
+
     print("Dirty repositories")
     for repo in dirty_repos:
         print("\t" + repo)
@@ -124,9 +129,6 @@ def walkbf(path: str) -> None:
     print("Clean repositories")
     for repo in clean_repos:
         print("\t" + repo)
-    print("Symlinks")
-    for repo in symlinks:
-        print("\t" + repo)
 
 
-walkbf('/home/lukas/')
+walkbf('/home/lukas')
