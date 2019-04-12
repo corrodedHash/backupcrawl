657389d389168ce23d8174126683f168c51b67a9
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2019-04-13 01:14:47 +0200

    Linting

--- main.py
+++ main.py
@@ -16,26 +16,26 @@ def walkbf(path: str) -> None:
                                    "/home/lukas/.vim/bundle", ]))
     sync_tree, git_repos, pacman_files = backupcrawler.scan(
         Path(path), ignore_paths=ignore_paths)
-    for current_file in sync_tree:
-        print("\t" + str(current_file))
+    for standard_file in sync_tree:
+        print("\t" + str(standard_file))
 
     for enum_state, status_string in (
             (GitSyncStatus.DIRTY, "Dirty repositories"),
             (GitSyncStatus.AHEAD, "Unsynced repositories"),
             (GitSyncStatus.CLEAN_SYNCED, "Clean repositories")):
         print(status_string + ":")
-        for current_file in [t.path for t in git_repos
-                             if t.status == enum_state]:
-            print("\t" + str(current_file))
+        for git_dir in [t.path for t in git_repos
+                        if t.status == enum_state]:
+            print("\t" + str(git_dir))
 
     print("Managed by pacman:")
     for pacman_status, status_string in (
             (PacmanSyncStatus.CHANGED, "Changed pacman files"),
             (PacmanSyncStatus.CLEAN, "Clean pacman files")):
         print(status_string + ":")
-        for current_file in [t for t in pacman_files
-                             if t.status == pacman_status]:
-            print("\t" + str(current_file.path) + " " + current_file.package)
+        for pacman_file in [t for t in pacman_files
+                            if t.status == pacman_status]:
+            print("\t" + str(pacman_file.path) + " " + pacman_file.package)
 
 
 logging.basicConfig(level="DEBUG")
